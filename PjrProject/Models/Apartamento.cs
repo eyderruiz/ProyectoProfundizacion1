00bd067e779c48ee970dd87ab1b46788cb106a83
Daikiria-eng <blackincolornegro@gmail.com>
2026-02-23 08:31:04 -0500

    Implementación de gestión de mensajería

---
+++ PjrProject/Models/Apartamento.cs
@@ -0,0 +1,11 @@
+﻿namespace PjrProject.Models
+{
+    public class Apartamento
+    {
+        public long UnidadId { get; set; }
+        public Torre TorreId { get; set; }
+        public long Numero { get; set; }
+        public string Tipo { get; set; }
+        public Double Area { get; set; }
+    }
+}
---
+++ PjrProject/Models/Torre.cs
@@ -0,0 +1,9 @@
+﻿namespace PjrProject.Models
+{
+    public class Torre
+    {
+        public long Id { get; set; }
+        public long ConjuntoId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
